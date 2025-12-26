fadcabe0d36d961529083e38483e434a94c48031
Giorgi Maziashvili <giorgim@redberry.ge>
2025-12-26 06:29:39 +0400

    feat: update development port and add new dependencies
    - Changed the development server port from 3000 to 3001 in package.json.
    - Added dependencies for Emotion, MUI, Axios, and TanStack React Query.
    - Integrated QueryClientProvider in main.tsx to manage query states.
    - Generated route tree in routeTree.gen.ts for routing management.
    - Created a new test route in test.tsx that fetches data using Axios and displays it.

---
+++ src/routes/test.tsx
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query'
+import { createFileRoute } from '@tanstack/react-router'
+import axios from 'axios'
+
+export const Route = createFileRoute('/test')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+    const { isPending, error, data } = useQuery({
+        queryKey: ['repoData'],
+        queryFn: () =>
+        axios.get('https://ie-manager-back.onrender.com/api/notion/connect',{
+            headers: {
+                "x-user-id": "user_123"
+            }
+        }).then((res) => {
+            console.log(res.data)
+        }),
+    })
+
+    if (isPending) return 'Loading...'
+
+    if (error) return 'An error has occurred: ' + error.message
+    return <div>Hello "/test"!</div>
+}
