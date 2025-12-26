245f7cd8723ed413c24c2efe04883d3dffae8d59
Giorgi Maziashvili <giorgim@redberry.ge>
2025-12-26 06:33:55 +0400

    feat: implement data fetching with useQuery in App component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,29 @@
 import { createFileRoute } from '@tanstack/react-router'
 import logo from '../logo.svg'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
+  const { isPending, error, data } = useQuery({
+      queryKey: ['repoData'],
+      queryFn: () =>
+      axios.get('https://ie-manager-back.onrender.com/api/notion/connect',{
+          headers: {
+              "x-user-id": "user_123"
+          }
+      }).then((res) => {
+          console.log(res.data)
+      }),
+  })
+  console.log(data)
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
   return (
     <div className="text-center">
       <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
